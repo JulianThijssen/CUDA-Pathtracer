784e78bea1b19a32d96a6d0e0abd05eba65c5883
Julian Thijssen <Nimthora@gmail.com>
2018-01-22 19:10:51 +0100

    Move BRDF functions to own file

--- Source/kernel.cu
+++ Source/kernel.cu
@@ -8,6 +8,7 @@
 #include <string>
 
 #include "Ray.h"
+#include "BRDF.h"
 
 #define PI 3.14159265
 #define ONE_OVER_PI 0.318309886
@@ -97,55 +98,6 @@ __global__ void setup_kernel(curandState *state) {
     curand_init(0, idx, 0, &state[idx]);
 }
 
-/* Calculates the diffuse contribution of the light */
-__device__ float CosTheta(Vector3f N, Vector3f L) {
-    return gmax(0, dot(N, L));
-}
-
-__device__ Vector3f Fresnel(Vector3f BaseColor, float Metalness, float b) {
-    Vector3f F0 = mix(Vector3f(0.04), BaseColor, Metalness);
-    return F0 + (Vector3f(1.0) - F0) * pow(1.0 - b, 5.0);
-}
-
-__device__ float GGX(float NdotH, float Roughness) {
-    float a = Roughness * Roughness;
-    float a2 = a * a;
-    float d = NdotH * NdotH * (a2 - 1.0) + 1.0;
-    return a2 / (PI * d * d);
-}
-
-__device__ float Schlick(float NdotL, float NdotV, float Roughness) {
-    float a = Roughness + 1.0;
-    float k = a * a * 0.125;
-    float G1 = NdotL / (NdotL * (1.0 - k) + k);
-    float G2 = NdotV / (NdotV * (1.0 - k) + k);
-    return G1 * G2;
-}
-
-__device__ Vector3f Lambert(Vector3f Kd) {
-    return Kd / PI;
-}
-
-__device__ Vector3f CookTorrance(Vector3f N, Vector3f V, Vector3f H, Vector3f L, Vector3f BaseColor, float Metalness, float Roughness) {
-    float NdotH = gmax(0.0f, dot(N, H));
-    float NdotV = gmax(1e-7f, dot(N, V));
-    float NdotL = gmax(1e-7f, dot(N, L));
-    float VdotH = gmax(0.0f, dot(V, H));
-
-    float D = GGX(NdotH, Roughness);
-    float G = Schlick(NdotL, NdotV, Roughness);
-    Vector3f F = Fresnel(BaseColor, Metalness, VdotH);
-
-    return (F * D * G) / (4.0 * NdotL * NdotV);
-}
-
-__device__ Vector3f BRDF(Vector3f N, Vector3f L, Material& material) {
-    float cos = CosTheta(N, L);
-    Vector3f LambertBRDF = Lambert(material.albedo);
-    //Vector3f CookBRDF = CookTorrance(info.n, V, H, L, mat.albedo, 0, 1);
-
-    return (LambertBRDF);
-}
 
 __device__ Vector3f directIllumination(const GPU_Scene& scene, Vector3f x, HitInfo info, const unsigned int idx, curandState *state) {
     Vector3f Radiance;
